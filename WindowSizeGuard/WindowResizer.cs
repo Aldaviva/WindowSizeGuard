4f19a6f0acd769d3709997e14c5024d898733f4e
Ben Hutchison <ben@aldaviva.com>
2020-08-04 17:47:05 -0700

    Only automatically resize windows that are visible, since some programs appear as normal and invisible instead of just minimized, like TagScanner and I think Outlook when hidden in the notification area.

--- WindowSizeGuard/WindowResizer.cs
+++ WindowSizeGuard/WindowResizer.cs
@@ -7,7 +7,6 @@ using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using ManagedWinapi.Windows;
 using Microsoft.Win32;
-using NLog;
 
 namespace WindowSizeGuard {
 
@@ -36,30 +35,28 @@ namespace WindowSizeGuard {
     [Component]
     public class WindowResizerImpl: WindowResizer {
 
-        private static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
-
-        private const           int  TWIPS_PER_INCH  = 15;
+        private const int TWIPS_PER_INCH = 15;
         private static readonly bool GAPLESS_WINDOWS = true;
 
         // can't find a good way to detect this programmatically, so whitelist them
         // to blacklist a title suffix, use titlePattern: new Regex(@"^.*(?<! ‎- OneNote for Windows 10)$")
         // these are all case-sensitive
-        private static readonly IEnumerable<WindowName> WINDOWS_WITH_NO_PADDING = new[] {
-            new WindowName(className: "XLMAIN"),         //Excel
-            new WindowName(className: "OpusApp"),        //Word
-            new WindowName(className: "rctrl_renwnd32"), //Outlook
-            new WindowName(className: "PPTFrameClass"),  //PowerPoint
-            new WindowName(executableBaseName: "devenv.exe"),
-            new WindowName(className: "Chrome_WidgetWin_1"), //Chromium programs like Vivaldi and Logitech G Hub
-            new WindowName(title: "Epic Games Launcher"),
-            new WindowName(className: "vguiPopupWindow"), //Steam
-            new WindowName(className: "Photoshop"),
-            new WindowName(className: "illustrator"),
-            new WindowName(className: "indesign"),
-            new WindowName(className: "_macr_dreamweaver_frame_window_"),
-            new WindowName(title: "TagScanner"),
-            new WindowName(className: "ESET Main Frame"),
-            new WindowName(className: "MozillaWindowClass")
+        private static readonly IEnumerable<WindowSelector> WINDOWS_WITH_NO_PADDING = new[] {
+            new WindowSelector(className: "XLMAIN"),              //Excel
+            new WindowSelector(className: "OpusApp"),             //Word
+            new WindowSelector(className: "rctrl_renwnd32"),      //Outlook
+            new WindowSelector(className: "PPTFrameClass"),       //PowerPoint
+            new WindowSelector(executableBaseName: "devenv.exe"), //Visual Studio
+            new WindowSelector(className: "Chrome_WidgetWin_1"),  //Chromium programs like Vivaldi and Logitech G Hub
+            new WindowSelector(className: "vguiPopupWindow"),     //Steam
+            new WindowSelector(title: "Epic Games Launcher"),
+            new WindowSelector(className: "Photoshop"),
+            new WindowSelector(className: "illustrator"),
+            new WindowSelector(className: "indesign"),
+            new WindowSelector(className: "_macr_dreamweaver_frame_window_"),
+            new WindowSelector(title: "TagScanner"),
+            new WindowSelector(className: "ESET Main Frame"),
+            new WindowSelector(className: "MozillaWindowClass")
         };
 
         private static readonly RECT NO_PADDING = new RECT(0, 0, 0, 0);
@@ -74,7 +71,11 @@ namespace WindowSizeGuard {
             const int BORDER_LINE_THICKNESS = 1;
 
             if (GAPLESS_WINDOWS) {
-                defaultPadding = new RECT(totalPadding + BORDER_LINE_THICKNESS, BORDER_LINE_THICKNESS, totalPadding + BORDER_LINE_THICKNESS, totalPadding + BORDER_LINE_THICKNESS);
+                defaultPadding = new RECT(
+                    totalPadding + BORDER_LINE_THICKNESS,
+                    BORDER_LINE_THICKNESS,
+                    totalPadding + BORDER_LINE_THICKNESS,
+                    totalPadding + BORDER_LINE_THICKNESS);
             } else {
                 defaultPadding = new RECT(totalPadding, 0, totalPadding, totalPadding);
             }
@@ -96,18 +97,18 @@ namespace WindowSizeGuard {
         public RECT getWindowPadding(SystemWindow window) => isWindowWithNoPadding(window) ? NO_PADDING : defaultPadding;
 
         internal static bool isWindowWithNoPadding(SystemWindow window) {
-            return (from candidate in WINDOWS_WITH_NO_PADDING
-                    where (candidate.className?.Equals(window.ClassName) ?? true) &&
-                          (candidate.titlePattern?.IsMatch(window.Title) ?? true) &&
-                          (candidate.executableBaseNameWithoutExeExtension?.Equals(window.Process.ProcessName) ?? true)
-                    select candidate).Any();
+            return (from selector in WINDOWS_WITH_NO_PADDING
+                    where (selector.className?.Equals(window.ClassName) ?? true) &&
+                          (selector.titlePattern?.IsMatch(window.Title) ?? true) &&
+                          (selector.executableBaseNameWithoutExeExtension?.Equals(window.Process.ProcessName) ?? true)
+                    select selector).Any();
         }
 
         public void moveWindowToPosition(SystemWindow window, RECT newPosition) => window.Position = newPosition;
 
         public bool canWindowBeManuallyResized(SystemWindow window) => window.Resizable;
 
-        public bool canWindowBeAutomaticallyResized(SystemWindow window) => window.Resizable && window.WindowState == FormWindowState.Normal;
+        public bool canWindowBeAutomaticallyResized(SystemWindow window) => window.Resizable && window.VisibilityFlag && (window.WindowState == FormWindowState.Normal);
 
         public IEnumerable<SystemWindow> findResizableWindows(SystemWindow? parent = null, int depth = 1) {
             if (depth <= 0) {
@@ -148,19 +149,19 @@ namespace WindowSizeGuard {
 
     }
 
-    internal readonly struct WindowName {
+    internal readonly struct WindowSelector {
 
         public readonly string? executableBaseNameWithoutExeExtension;
         public readonly string? className;
-        public readonly Regex?  titlePattern;
+        public readonly Regex? titlePattern;
 
-        public WindowName(string? executableBaseName = null, string? className = null, string? title = null, Regex? titlePattern = null) {
+        public WindowSelector(string? executableBaseName = null, string? className = null, string? title = null, Regex? titlePattern = null) {
             if (titlePattern != null && title != null) {
                 throw new ArgumentException("Please specify at most 1 of the titlePattern and title arguments, not both.");
             }
 
-            executableBaseNameWithoutExeExtension = executableBaseName != null ? Regex.Replace(executableBaseName, @"\.exe$", string.Empty, RegexOptions.IgnoreCase) : null;
             this.className                        = className;
+            executableBaseNameWithoutExeExtension = executableBaseName != null ? Regex.Replace(executableBaseName, @"\.exe$", string.Empty, RegexOptions.IgnoreCase) : null;
 
             if (titlePattern != null) {
                 this.titlePattern = titlePattern;
